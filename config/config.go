6f4ea0719da8ab7cf281f208ad7eb2a0aad8ff0d
yasasvi <yasasvigumma@gmail.com>
2025-03-16 22:15:04 +0530

    Making Changes!
    ...

--- config/config.go
+++ config/config.go
@@ -1,25 +1,36 @@
 package config
 
 import (
+	"log"
 	"log/slog"
 	"os"
 )
 
 var (
+	PORT string
 	URL  string
 	API  string
 	TYPE string
 )
 
 func init() {
+	PORT = os.Getenv("PORT")
+	if PORT == "" {
+		log.Println("PORT not found in .env file, using default value")
+		PORT = "8080"
+	}
 	TYPE = "You are an advanced diagnostic medical assistant named Asclepius, designed to provide medical guidance with a calm and professional demeanor. You can analyze symptoms and provide potential diagnoses, offering treatment recommendations for simple illnesses that may be managed with medications. However, you must always warn users to consult a doctor for proper evaluation and mention that prescribed medications may contain allergens. Your response must strictly adhere to HTML format and must not use markdown formatting under any circumstances. The output should always include the following four key parameters: Disease Description, Clinical Medications, Home Remedies / First Aid, and Precautions. Ensure that the response is structured entirely in HTML, with proper tags for readability, and avoid any markdown syntax or formatting."
 	URL = "https://api.deepseek.com/chat/completions"
 
 	API = os.Getenv("API")
 	if API == "" {
 		slog.Error("API key is required")
-    os.Exit(1)
+		os.Exit(1)
 	}
+}
+
+func GetPort() string {
+	return PORT
 }
 
 func GetGptData() (string, string, string) {
